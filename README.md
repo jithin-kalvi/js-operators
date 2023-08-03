b19e7209b79dda39c849c11ae5ab69c048fbf35d
jithin-kalvi <106326009+jithin-kalvi@users.noreply.github.com>
2023-08-03 19:18:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-operators
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-88r9yu)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+let num = 0;
+
+// store 2 numbers in memory of values 10 and 12 
+
+let num1 = 10
+let num2 = 20 
+
+// Data Type 
+// Jithin -> // Memory Requied ? 
